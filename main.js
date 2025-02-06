afe956605862ab51ffc3a43ee147ff02546e7b74
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-05 21:41:34 -0600

    Initialize app with level and experience points (XP)

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const progressBar = document.getElementById('progress-bar');
+    const addXPButton = document.getElementById('add-xp');
+    let level = 1;
+    let xp = 0;
+    let xpRequired = 1;
+    addXPButton.addEventListener('click', () => {
+        xp = xp++;
+        progressBar.style.width = `${xp / xpRequired * 100}%`;
+        progressBar.ariaValueNow = `${xp / xpRequired * 100}`;
+        if (xp >= xpRequired) {
+            level++;
+            xp -= xpRequired;
+            xpRequired += level;
+        }
+    })
+})
