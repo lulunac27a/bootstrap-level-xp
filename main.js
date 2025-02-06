cfc5315124678307936a746d87c89fbf221fa935
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-05 21:48:05 -0600

    Format code

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const progressBar = document.getElementById('progress-bar');
     const addXPButton = document.getElementById('add-xp');
     let level = 1;
     let xp = 0;
     let xpRequired = 1;
-    addXPButton.addEventListener('click', function() {
+    addXPButton.addEventListener('click', function () {
         xp = xp++;
         progressBar.style.width = `${xp / xpRequired * 100}%`;
         progressBar.ariaValueNow = `${xp / xpRequired * 100}`;
