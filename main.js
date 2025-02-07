1f858669793e072786f42e5fed853a1449cdf33b
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-06 22:49:45 -0600

    Add title

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-document.addEventListener("DOMContentLoaded", function init() {
-    const progressBar = document.getElementById("progress-bar");
-    const addXPButton = document.getElementById("add-xp");
+document.addEventListener('DOMContentLoaded', function init() {
+    const progressBar = document.getElementById('progress-bar');
+    const addXPButton = document.getElementById('add-xp');
     let level = 1;
     let xp = 0;
     let xpRequired = 1;
-    addXPButton.addEventListener("click", function addXP() {
+    addXPButton.addEventListener('click', function addXP() {
         xp++;
-        progressBar.style.width = `${(xp / xpRequired) * 100}%`;
-        progressBar.ariaValueNow = `${(xp / xpRequired) * 100}`;
+        progressBar.style.width = `${xp / xpRequired * 100}%`;
+        progressBar.ariaValueNow = `${xp / xpRequired * 100}`;
         if (xp >= xpRequired) {
             level++;
             progressBar.textContent = `$Level {level}`;
             xp -= xpRequired;
             xpRequired += level;
         }
-    });
-});
+    })
+})
