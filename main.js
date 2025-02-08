04096f40a5abc0d642322a2c5e180007d86044ab
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-07 20:11:47 -0600

    Format code

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
-document.addEventListener('DOMContentLoaded', function init() {//initialize app when webpage is loaded
-    const progressBar = document.getElementById('progress-bar');//progress bar
-    const addXPButton = document.getElementById('add-xp');//add XP button
-    let level = 1;//level
-    let xp = 0;//XP
-    let xpRequired = 1;//XP required to level up
-    addXPButton.addEventListener('click', function addXP() {//event to add XP
-        xp++;//add 1 XP
-        progressBar.style.width = `${xp / xpRequired * 100}%`;//update progress bar width
-        progressBar.ariaValueNow = `${xp / xpRequired * 100}`;//update progress bar aria value
-        if (xp >= xpRequired) {//check if XP is greater than or equal to XP required
-            level++;//increase level
-            progressBar.textContent = `$Level {level}`;//update level text
-            xp -= xpRequired;//decrease XP by XP required
-            xpRequired += level;//increase XP required by level
+document.addEventListener("DOMContentLoaded", function init() {
+    //initialize app when webpage is loaded
+    const progressBar = document.getElementById("progress-bar"); //progress bar
+    const addXPButton = document.getElementById("add-xp"); //add XP button
+    let level = 1; //level
+    let xp = 0; //XP
+    let xpRequired = 1; //XP required to level up
+    addXPButton.addEventListener("click", function addXP() {
+        //event to add XP
+        xp++; //add 1 XP
+        progressBar.style.width = `${(xp / xpRequired) * 100}%`; //update progress bar width
+        progressBar.ariaValueNow = `${(xp / xpRequired) * 100}`; //update progress bar aria value
+        if (xp >= xpRequired) {
+            //check if XP is greater than or equal to XP required
+            level++; //increase level
+            progressBar.textContent = `$Level {level}`; //update level text
+            xp -= xpRequired; //decrease XP by XP required
+            xpRequired += level; //increase XP required by level
         }
-    })
-})
+    });
+});
