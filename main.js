07046310819be1eb6b47ac9f74b843063b9c379d
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2025-02-08 13:36:54 -0600

    Add level text display

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function init() {
     //initialize app when webpage is loaded
     const progressBar = document.getElementById("progress-bar"); //progress bar
+    const levelText = document.getElementById("level"); //level text
     const addXPButton = document.getElementById("add-xp"); //add XP button
     let level = 1; //set level to 1
     let xp = 0; //set XP to 0
@@ -14,6 +15,7 @@ document.addEventListener("DOMContentLoaded", function init() {
             //check if XP is greater than or equal to XP required
             level++; //increase level
             progressBar.textContent = `$Level {level}`; //update level text
+            levelText.textContent = `$Level {level}`; //update level text
             xp -= xpRequired; //decrease XP by XP required
             xpRequired += level; //increase XP required by level
         }
